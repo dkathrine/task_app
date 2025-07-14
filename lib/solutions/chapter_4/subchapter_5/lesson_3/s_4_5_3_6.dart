38a594948a023d0fb90ebd3dad0ad9e4934aeea9
Kathrine <101878099+dkathrine@users.noreply.github.com>
2025-07-14 13:58:07 +0200

    solved task 4.5.3.6 in task app

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -2,8 +2,35 @@ import 'package:flutter/material.dart';
 
 class S4536 extends StatelessWidget {
   const S4536({super.key});
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    final List<Product> products = [
+      Product(name: 'Bread', price: 2.30),
+      Product(name: 'Butter', price: 2.99),
+      Product(name: 'Cheese', price: 1.99),
+      Product(name: 'Milk', price: 1.99),
+      Product(name: 'Pepper', price: 2.79)
+    ];
+
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        final product = products[index];
+        return ListTile(
+          leading: const Icon(Icons.shopping_cart),
+          title: Text(product.name),
+          subtitle: Text('${product.price.toStringAsFixed(2)} €'),
+        );
+      },
+    );
   }
+}
+
+class Product {
+  Product({required this.name, required this.price});
+
+  final String name;
+  final double price;
 }
