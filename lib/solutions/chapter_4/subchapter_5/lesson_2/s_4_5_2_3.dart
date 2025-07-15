56ae887f9383b69e470d1199fba70f302b50634e
Kathrine <101878099+dkathrine@users.noreply.github.com>
2025-07-15 11:30:55 +0200

    solved tasks 4.5.2.3 & 4.5.2.4 in task app

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,31 @@ class S4523 extends StatelessWidget {
   const S4523({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  const MyNameWidget({super.key});
+
+  @override
+  State<MyNameWidget> createState() => _MyNameWidgetState();
+}
+
+class _MyNameWidgetState extends State<MyNameWidget> {
+  String _name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Text(_name),
+      ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _name = 'Kathrine';
+            });
+          },
+          child: Text('Name anzeigen'))
+    ]);
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,37 @@ class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ShowHideNameWidget();
+  }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String _name = '';
+  String _title = 'Name anzeigen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(_name),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _name = _name == '' ? 'Kathrine' : '';
+                _title = _title == 'Name anzeigen'
+                    ? 'Name verstecken'
+                    : 'Name anzeigen';
+              });
+            },
+            child: Text(_title))
+      ],
+    );
   }
 }
